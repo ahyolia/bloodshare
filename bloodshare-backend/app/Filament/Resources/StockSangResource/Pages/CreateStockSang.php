a65f08d62466a539bc5fd0ab373ceea25ebd0caf
Camélia AMIN HANDOYO <camhdyo@gmail.com>
2026-06-22 16:53:32 +1100

    feat(Ressources Filament) : création du CRUD stock de sang

---
+++ bloodshare-backend/app/Filament/Resources/StockSangResource/Pages/CreateStockSang.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\StockSangResource\Pages;
+
+use App\Filament\Resources\StockSangResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateStockSang extends CreateRecord
+{
+    protected static string $resource = StockSangResource::class;
+}
