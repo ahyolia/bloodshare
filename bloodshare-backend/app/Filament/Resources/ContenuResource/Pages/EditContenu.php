5d7b050b868c41e61482770c15b530f6e4f22483
Camélia AMIN HANDOYO <camhdyo@gmail.com>
2026-07-07 14:58:32 +1100

    feat(backoffice): ressources Evenements et Contenus

---
+++ bloodshare-backend/app/Filament/Resources/ContenuResource/Pages/EditContenu.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\ContenuResource\Pages;
+
+use App\Filament\Resources\ContenuResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditContenu extends EditRecord
+{
+    protected static string $resource = ContenuResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
